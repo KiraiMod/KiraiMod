c13fca31766fa35641359421734bbee6b03cce5f
xKiraiChan <xKiraiChan@gmail.com>
2022-02-06 21:44:26 -0500

    initial framework

---
+++ KiraiMod/KiraiMod.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+
+namespace KiraiMod
+{
+    [BepInPlugin("me.kiraihooks.KiraiMod", "KiraiMod", "2.0.0.0")]
+    public class KiraiMod : BasePlugin
+    {
+        public override void Load()
+        {
+            Shared.Logger = Log;
+        }
+
+        public override bool Unload()
+        {
+            return base.Unload();
+        }
+    }
+}
---
+++ KiraiMod/Shared.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiraiMod
+{
+    public static class Shared
+    {
+        public static BepInEx.Logging.ManualLogSource Logger;
+    }
+}
