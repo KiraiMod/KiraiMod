6c3e861396d4c732a25c195331a04c76a34d833c
xKiraiChan <xKiraiChan@gmail.com>
2022-02-27 23:48:00 -0500

    add more movement stuff, On extension, clean gui glue

--- KiraiMod/GUI/Movement.cs
+++ KiraiMod/GUI/Movement.cs
@@ -10,8 +10,10 @@ namespace KiraiMod.GUI
         public static Toggle flight;
         public static Toggle directional;
         public static Toggle noclip;
-
         public static KiraiSlider flightSpeed;
+        public static Button locomotion;
+        public static Button position_save;
+        public static Button position_load;
 
         public static void Setup(Transform self)
         {
@@ -21,10 +23,13 @@ namespace KiraiMod.GUI
                 .Dragable()
                 .Closable();
 
-            (flight = Body.Find("Flight").GetComponent<Toggle>()).onValueChanged.AddListener(new Action<bool>(state => Modules.Movement.State = state));
-            (directional = Body.Find("Directional").GetComponent<Toggle>()).onValueChanged.AddListener(new Action<bool>(state => Modules.Movement.directional.Value = state));
-            (noclip = Body.Find("NoClip").GetComponent<Toggle>()).onValueChanged.AddListener(new Action<bool>(state => Modules.Movement.noclip.Value = state));
-            (flightSpeed = Body.Find("FlightSpeed").GetKiraiSlider()).slider.onValueChanged.AddListener(new Action<float>(value => Modules.Movement.speed.Value = value));
+            Body.Find("Flight").GetComponent<Toggle>().On(state => Modules.Movement.State = state);
+            Body.Find("Directional").GetComponent<Toggle>().On(state => Modules.Movement.directional.Value = state);
+            Body.Find("NoClip").GetComponent<Toggle>().On(state => Modules.Movement.noclip.Value = state);
+            Body.Find("FlightSpeed").GetKiraiSlider().slider.On(value => Modules.Movement.speed.Value = value);
+            Body.Find("Locomotion").GetComponent<Button>().On(Modules.Movement.UseLegacyLocomotion);
+            Body.Find("Position/Save").GetComponent<Button>().On(Modules.Movement.SavePosition);
+            Body.Find("Position/Load").GetComponent<Button>().On(Modules.Movement.LoadPosition);
 
             Modules.Movement.directional.GUIBind(directional);
             Modules.Movement.noclip.GUIBind(noclip);
--- KiraiMod/Modules/Movement.cs
+++ KiraiMod/Modules/Movement.cs
@@ -12,7 +12,7 @@ namespace KiraiMod.Modules
     public static class Movement
     {
         public static readonly ConfigEntry<bool> directional = Shared.Config.Bind("Flight", "Directional", false, "Should you move in the direction you are looking");
-        public static readonly ConfigEntry<bool> noclip /**/ = Shared.Config.Bind("Flight", "NoClip", false," Should you be able to go through solid objects");
+        public static readonly ConfigEntry<bool> noclip /**/ = Shared.Config.Bind("Flight", "NoClip", false, " Should you be able to go through solid objects");
         public static readonly ConfigEntry<float> speed /**/ = Shared.Config.Bind("Flight", "Speed", 8.0f, "The speed in meters per second at which you fly");
         public static readonly ConfigEntry<Key[]> keybind = Shared.Config.Bind("Flight", "keybind", new Key[] { Key.LeftCtrl, Key.F }, "The keybind to toggle flight");
 
@@ -25,96 +25,107 @@ namespace KiraiMod.Modules
                 if (state == value) return;
                 GUI.Movement.flight.Set(state = value, false);
 
-                if (value) Enable();
-                else Disable();
+                if (value) Flight.Enable();
+                else Flight.Disable();
             }
         }
 
         static Movement()
         {
             keybind.RegisterKeybind(() => State ^= true);
-            
+
             noclip.SettingChanged += (sender, args) => Collisions.Set(noclip.Value);
             directional.SettingChanged += (sender, args) => Target.Fetch();
 
             typeof(Hooks).Initialize();
         }
 
-        private static Vector3 oGrav = new(0, -9.8f, 0);
+        // in the future this may have more complicated logic
+        public static void UseLegacyLocomotion() => Networking.LocalPlayer.UseLegacyLocomotion();
 
-        private static void Enable()
+        private static Vector3 prevPos;
+        public static void SavePosition() => prevPos = Networking.LocalPlayer.gameObject.transform.position;
+        public static void LoadPosition() => Networking.LocalPlayer.gameObject.transform.position = prevPos;
+
+        private static class Flight
         {
-            Physics.gravity = Vector3.zero;
+            public static Vector3 oGrav = new(0, -9.8f, 0);
 
-            if (XRDevice.isPresent)
-                Events.Update += UpdateVR;
-            else Events.Update += UpdateDesktop;
-            Events.WorldLoaded += WorldLoaded;
-            if (noclip.Value)
-                Collisions.Set(true);
-            Target.Fetch();
-        }
-
-        private static void Disable()
-        {
-            if (XRDevice.isPresent)
-                Events.Update -= UpdateVR;
-            else Events.Update -= UpdateDesktop;
-            Events.WorldLoaded -= WorldLoaded;
-            Collisions.Set(false);
-
-            Physics.gravity = oGrav;
-        }
-
-        private static void WorldLoaded(UnityEngine.SceneManagement.Scene scene) => Events.Update += UpdateCheck;
-        private static void UpdateCheck()
-        {
-            if (Networking.LocalPlayer == null)
-                return;
-
-            Events.Update -= UpdateCheck;
-
-            if (noclip.Value)
-                Collisions.Set(true);
-            Target.Fetch();
-        }
-
-        private static void UpdateDesktop()
-        {
-            if (Networking.LocalPlayer == null) return;
-
-            unsafe
+            public static void Enable()
             {
-                byte shift = ToByte(Input.GetKey(KeyCode.LeftShift));
+                Physics.gravity = Vector3.zero;
+
+                if (XRDevice.isPresent)
+                    Events.Update += UpdateVR;
+                else Events.Update += UpdateDesktop;
+                Events.WorldLoaded += WorldLoaded;
+                if (noclip.Value)
+                    Collisions.Set(true);
+                Target.Fetch();
+            }
+
+            public static void Disable()
+            {
+                if (XRDevice.isPresent)
+                    Events.Update -= UpdateVR;
+                else Events.Update -= UpdateDesktop;
+                Events.WorldLoaded -= WorldLoaded;
+                Collisions.Set(false);
+
+                Physics.gravity = oGrav;
+            }
+
+            private static void WorldLoaded(UnityEngine.SceneManagement.Scene scene) => Events.Update += UpdateCheck;
+
+            private static void UpdateCheck()
+            {
+                if (Networking.LocalPlayer == null)
+                    return;
+
+                Events.Update -= UpdateCheck;
+
+                if (noclip.Value)
+                    Collisions.Set(true);
+                Target.Fetch();
+            }
+
+            private static void UpdateDesktop()
+            {
+                if (Networking.LocalPlayer == null) return;
+
+                unsafe
+                {
+                    byte shift = ToByte(Input.GetKey(KeyCode.LeftShift));
+                    float _speed = speed.Value;
+
+                    Networking.LocalPlayer.gameObject.transform.position +=
+                        Target.value.forward * _speed * Time.deltaTime *
+                            (ToByte(Input.GetKey(KeyCode.W)) + ~ToByte(Input.GetKey(KeyCode.S)) + 1) * (shift * 8 + 1)
+                        + Target.value.right * _speed * Time.deltaTime *
+                            (ToByte(Input.GetKey(KeyCode.D)) + ~ToByte(Input.GetKey(KeyCode.A)) + 1) * (shift * 8 + 1)
+                        + Target.value.up * _speed * Time.deltaTime *
+                            (ToByte(Input.GetKey(KeyCode.E)) + ~ToByte(Input.GetKey(KeyCode.Q)) + 1) * (shift * 8 + 1);
+                }
+
+                Networking.LocalPlayer.SetVelocity(new Vector3(0f, 0f, 0f));
+            }
+
+            private static void UpdateVR()
+            {
+                if (Networking.LocalPlayer == null) return;
                 float _speed = speed.Value;
 
                 Networking.LocalPlayer.gameObject.transform.position +=
-                    Target.value.forward * _speed * Time.deltaTime *
-                        (ToByte(Input.GetKey(KeyCode.W)) + ~ToByte(Input.GetKey(KeyCode.S)) + 1) * (shift * 8 + 1)
-                    + Target.value.right * _speed * Time.deltaTime *
-                        (ToByte(Input.GetKey(KeyCode.D)) + ~ToByte(Input.GetKey(KeyCode.A)) + 1) * (shift * 8 + 1)
-                    + Target.value.up * _speed * Time.deltaTime *
-                        (ToByte(Input.GetKey(KeyCode.E)) + ~ToByte(Input.GetKey(KeyCode.Q)) + 1) * (shift * 8 + 1);
+                    Target.value.forward * _speed * Time.deltaTime * Input.GetAxis("Vertical")
+                    + Target.value.right * _speed * Time.deltaTime * Input.GetAxis("Horizontal")
+                    + Target.value.up * _speed * Time.deltaTime * Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical");
+
+                Networking.LocalPlayer.SetVelocity(new Vector3(0f, 0f, 0f));
             }
 
-            Networking.LocalPlayer.SetVelocity(new Vector3(0f, 0f, 0f));
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            private static unsafe byte ToByte(bool from) => *(byte*)&from;
         }
-
-        private static void UpdateVR()
-        {
-            if (Networking.LocalPlayer == null) return;
-            float _speed = speed.Value;
-
-            Networking.LocalPlayer.gameObject.transform.position +=
-                Target.value.forward * _speed * Time.deltaTime * Input.GetAxis("Vertical")
-                + Target.value.right * _speed * Time.deltaTime * Input.GetAxis("Horizontal")
-                + Target.value.up * _speed * Time.deltaTime * Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical");
-
-            Networking.LocalPlayer.SetVelocity(new Vector3(0f, 0f, 0f));
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe byte ToByte(bool from) => *(byte*)&from;
 
         private static class Collisions
         {
@@ -157,7 +168,7 @@ namespace KiraiMod.Modules
                 if (__0.magnitude == 0)
                     return true;
 
-                oGrav = __0;
+                Flight.oGrav = __0;
                 return !state;
             }
         }
