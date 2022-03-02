11088089b13b09661ded96746f52295e2d2c6267
xKiraiChan <xKiraiChan@gmail.com>
2022-03-02 00:59:47 -0500

    loader now uses knames (e.g. Public/KiraiMod.dll)

--- KiraiMod.Loader/Loader.cs
+++ KiraiMod.Loader/Loader.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -16,7 +17,7 @@ namespace KiraiMod.Loader
     [BepInPlugin("me.kiraihooks.KiraiMod.Loader", "KM.Loader", "0.0.0")]
     public class Loader : BasePlugin
     {
-        public const string Server = "https://KiraiHooks.me/KiraiMod/Public/";
+        public const string Server = "https://KiraiHooks.me/KiraiMod/";
         private const string Updater = "KiraiMod.Updater.exe";
 
         internal static ManualLogSource Logger;
@@ -33,6 +34,14 @@ namespace KiraiMod.Loader
                 Config.Bind("Updating", "Safe", false, "Updating will halt if the server cannot be contacted").Value,
                 Config.Bind("Updating", "Slow", false, "Enabling will prevent the process from moving forward until the server responds").Value
             );
+
+            if ((GetAsyncKeyState('K') & 0x8000) > 0)
+            {
+                Log.LogMessage("Entering setup utility...");
+                Console.Beep();
+                Setup.Run(http, Log);
+                Log.LogInfo("Setup utility finished");
+            }
         }
 
         private void UpdateFiles(Dictionary<string, string> table)
@@ -53,26 +62,51 @@ namespace KiraiMod.Loader
             }
         }
 
-        private string[] FetchOutOfDate(Dictionary<string, string> table, string cd) => 
-            Directory.EnumerateFiles(cd, "KiraiMod.*")
-                .Select(x => (x, SHA256(Encoding.UTF8.GetBytes(Path.GetFileName(x) + ".hash"))))
-                .Where(x => table.ContainsKey(x.Item2))
-                .Select(x => (x.x, SHA256(File.ReadAllBytes(x.x)), table[x.Item2]))
+        private string[] FetchOutOfDate(Dictionary<string, string> table, string cd) =>
+            Directory.EnumerateFiles(cd, "*", SearchOption.AllDirectories)
+                .Select(x => (path: x, kname: GetKName(cd, x)))
+                .Select(x => (x.path, x.kname, nameHash: SHA256(Encoding.UTF8.GetBytes(x.kname + ".hash"))))
+                .Where(x => table.ContainsKey(x.nameHash))
+                .Select(x => (x.path, x.kname, oldHash: SHA256(File.ReadAllBytes(x.path)), newHash: table[x.nameHash]))
+                .Where(x => x.oldHash != x.newHash)
                 .Where(x =>
                 {
-                    if (x.Item2 == x.Item3) return false;
-
-                    string fname = Path.GetFileName(x.x);
-                    if (fname != Updater) return true;
+                    if (x.kname != "Public/" + Updater) return true;
                     else
                     {
-                        File.WriteAllBytes(x.x, http.GetByteArrayAsync(Server + fname).Result);
+                        File.WriteAllBytes(x.path, http.GetByteArrayAsync(Server + x.kname).Result);
                         return false;
                     }
                 })
-                .Select(x => Path.GetFileName(x.x))
+                .Select(x => x.kname)
                 .ToArray();
 
         private string SHA256(byte[] bytes) => string.Join("", new SHA256Managed().ComputeHash(bytes).Select(x => string.Format("{0:x2}", x)).ToArray());
+
+        [DllImport("user32.dll")]
+        private static extern int GetAsyncKeyState(int vKey);
+
+        private static string GetKName(string cd, string path)
+        {
+            string rel = GetRelativePath(cd, path);
+            int slashIdx = rel.IndexOf("\\");
+            if (slashIdx == -1 || slashIdx == rel.Length - 1) return $"Public/{rel}";
+            else return rel.Replace('\\', '/');
+        }
+
+        private static string GetRelativePath(string from, string to)
+        {
+            Uri _from = new(from);
+            Uri _to = new(to);
+
+            if (_from.Scheme != _to.Scheme) return to;
+
+            Uri relativeUri = _from.MakeRelativeUri(_to);
+            string relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            if (string.Equals(_to.Scheme, Uri.UriSchemeFile, StringComparison.OrdinalIgnoreCase))
+                return relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            else return relativePath;
+        }
     }
 }
