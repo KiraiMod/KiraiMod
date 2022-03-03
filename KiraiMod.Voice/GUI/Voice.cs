444711b0828e68acbe64c2db403903b6b5faff92
xKiraiChan <xKiraiChan@gmail.com>
2022-03-02 23:54:41 -0500

    voice, guibindfull, build script, vrcnc, minfo logas

---
+++ KiraiMod.Voice/GUI/Voice.cs
@@ -0,0 +1,25 @@
+﻿using KiraiMod.Core;
+using KiraiMod.GUI.Common;
+using KiraiMod.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KiraiMod.Voice.GUI
+{
+    public static class Voice
+    {
+        public static void Setup(Transform Self)
+        {
+            Transform Body = Self.Find("Body");
+
+            Window.Create(Self)
+                .Dragable()
+                .Closable(Body.gameObject)
+                .Pinnable();
+
+            Modules.Voice.LoudMic.GUIBindFull(Body.Find("LoudMic").GetComponent<Toggle>());
+            Modules.Voice.UtopiaVoice.GUIBindFull(Body.Find("UtopiaVoice").GetComponent<Toggle>());
+            Modules.Voice.UtopiaOnly.GUIBindFull(Body.Find("UtopiaOnly").GetComponent<Toggle>());
+        }
+    }
+}
---
+++ KiraiMod.Voice/Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using KiraiMod.Core;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace KiraiMod.Voice
+{
+    [BepInPlugin(GUID, "KM.Voice", "0.0.0")]
+    [BepInDependency("me.kiraihooks.KiraiMod", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("me.kiraihooks.KiraiMod.Core", BepInDependency.DependencyFlags.HardDependency)]
+    public class Plugin : BasePlugin
+    {
+        private const string GUID = "me.kiraihooks.KiraiMod.Voice";
+
+        internal static ConfigFile cfg;
+        internal static Harmony harmony;
+
+        private AssetBundle bundle;
+
+        public override void Load()
+        {
+            cfg = Config;
+            harmony = new(GUID);
+
+            Managers.ModuleManager.Register();
+            Managers.GUIManager.OnLoad += GUIManager_OnLoad;
+        }
+
+        private void GUIManager_OnLoad()
+        {
+            MemoryStream mem = new();
+            Assembly.GetExecutingAssembly().GetManifestResourceStream("KiraiMod.Voice.Lib.KiraiMod.Voice.GUI.AssetBundle").CopyTo(mem);
+            bundle = AssetBundle.LoadFromMemory(mem.ToArray());
+            bundle.hideFlags |= HideFlags.HideAndDontSave;
+
+            Transform GUI = bundle.LoadAsset("assets/voice.gui.prefab")
+                .Cast<GameObject>()
+                .Instantiate()
+                .transform;
+
+            for (int i = 0; i < GUI.childCount; i++)
+                GUI.GetChild(i).SetParent(Managers.GUIManager.UserInterface.transform);
+
+            GUI.Destroy();
+        }
+    }
+}
