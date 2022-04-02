cbb6f3a0e7bc8df63fc06bd31aeade3fc1b54f76
xKiraiChan <xKiraiChan@gmail.com>
2022-04-02 01:41:07 -0400

    Port voice over to new ui

--- KiraiMod.Voice/Plugin.cs
+++ KiraiMod.Voice/Plugin.cs
@@ -3,14 +3,10 @@ using BepInEx.Configuration;
 using BepInEx.IL2CPP;
 using HarmonyLib;
 using KiraiMod.Core;
-using System.IO;
-using System.Reflection;
-using UnityEngine;
 
 namespace KiraiMod.Voice
 {
     [BepInPlugin(GUID, "KM.Voice", "0.0.0")]
-    [BepInDependency("me.kiraihooks.KiraiMod", BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency("me.kiraihooks.KiraiMod.Core", BepInDependency.DependencyFlags.HardDependency)]
     public class Plugin : BasePlugin
     {
@@ -19,33 +15,12 @@ namespace KiraiMod.Voice
         internal static ConfigFile cfg;
         internal static Harmony harmony;
 
-        private AssetBundle bundle;
-
         public override void Load()
         {
             cfg = Config;
             harmony = new(GUID);
 
-            Managers.ModuleManager.Register();
-            Core.UI.LegacyGUIManager.OnLoad += GUIManager_OnLoad;
-        }
-
-        private void GUIManager_OnLoad()
-        {
-            MemoryStream mem = new();
-            Assembly.GetExecutingAssembly().GetManifestResourceStream("KiraiMod.Voice.Lib.KiraiMod.Voice.GUI.AssetBundle").CopyTo(mem);
-            bundle = AssetBundle.LoadFromMemory(mem.ToArray());
-            bundle.hideFlags |= HideFlags.HideAndDontSave;
-
-            Transform GUI = bundle.LoadAsset("assets/voice.gui.prefab")
-                .Cast<GameObject>()
-                .Instantiate()
-                .transform;
-
-            for (int i = 0; i < GUI.childCount; i++)
-                GUI.GetChild(i).SetParent(Core.UI.LegacyGUIManager.UserInterface.transform);
-
-            GUI.Destroy();
+            typeof(Voice).Initialize();
         }
     }
 }
