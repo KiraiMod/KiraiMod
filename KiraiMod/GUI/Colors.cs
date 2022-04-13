12744c7738a5272e72c0b3c50d5a94e77e567423
xKiraiChan <xKiraiChan@gmail.com>
2022-04-12 22:45:42 -0400

    optimize tracers

---
+++ KiraiMod/GUI/Colors.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace KiraiMod.GUI
+{
+    public static class Colors
+    {
+        public static Color32 Primary = new(0x56, 0x00, 0xA5, 0xFF);
+    }
+}
--- KiraiMod/Modules/Tracers.cs
+++ KiraiMod/Modules/Tracers.cs
@@ -9,64 +9,57 @@ namespace KiraiMod.Modules
     public static class Tracers
     {
         public static ConfigEntry<bool> PlayerTracers = Plugin.cfg.Bind("Tracers", "Players", false, "Should lines be drawn to every player in the world");
+
         public static GameObject tracers;
         public static TracersHelper instance;
-        //public static Camera cameraMain;
+        public static Camera cameraMain;
 
         static Tracers() 
         {
             Events.ApplicationStart += () => 
-            { 
-                //cameraMain = Camera.main; 
-                CreateGOTracers(); 
+            {
+                tracers = new GameObject("KiraiMod.Tracers").DontDestroyOnLoad();
+                instance = tracers.AddComponent<TracersHelper>();
+                PlayerTracers.SettingChanged += ((EventHandler)((sender, args) => instance.enabled = PlayerTracers.Value)).Invoke();
             };
+
+            Events.UIManagerLoaded += () => cameraMain = Camera.main;
+
             GUI.Groups.Loaded += () => GUI.Groups.Visuals.AddElement("Player Tracers", PlayerTracers.Value).Bound.Bind(PlayerTracers);
         }
 
-        public static void CreateGOTracers()
-        {
-            tracers = new GameObject("KiraiMod.Tracers").DontDestroyOnLoad();
-            instance = tracers.AddComponent<TracersHelper>();
-            PlayerTracers.SettingChanged += ((EventHandler)((sender, args) =>
-            {
-                instance.enabled = PlayerTracers.Value;
-            })).Invoke();
-
-        }
         public class TracersHelper : MonoBehaviour
         {
             static TracersHelper() => ClassInjector.RegisterTypeInIl2Cpp<TracersHelper>();
+
             public TracersHelper() : base(ClassInjector.DerivedConstructorPointer<TracersHelper>()) => ClassInjector.DerivedConstructorBody(this);
             public TracersHelper(IntPtr ptr) : base(ptr) { }
-
-            private static Material lineMaterial;
+            
             public void OnRenderObject()
             {
-                if (Networking.LocalPlayer == null) return;
-                if (Camera.current != Camera.main) return; //can be optimized by caching Camera.main & if statments can be collapsed into 1
+                if (Networking.LocalPlayer == null || Camera.current != cameraMain) return;
+                
+                // this can be optimized but it seems to be going null randomly
+                Material mat = new(Shader.Find("Hidden/Internal-Colored"));
+                mat.SetInt("_ZTest", 0);
+                mat.SetColor("Color", new Color(0x56, 0x00, 0xA5));
+                mat.renderQueue = 5000;
+                mat.SetPass(0);
+
+                Vector3 local = Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.RightHand).position;
+
                 foreach (VRCPlayerApi player in VRCPlayerApi.AllPlayers)
                 {
                     GL.PushMatrix();
-                    CreateLineMaterial();
-                    lineMaterial.SetPass(0);
                     GL.Begin(1);
-                    GL.Color(new Color(0x56, 0x00, 0xA5, 1));
-                    if (player.gameObject.transform.Find("ForwardDirection/Avatar") == null) return;
-                    GL.Vertex(player.gameObject.transform.Find("ForwardDirection/Avatar").position);
-                    GL.Vertex(Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.RightHand).position);
+                    // todo: make this color based on user info
+                    GL.Color(GUI.Colors.Primary);
+                    GL.Vertex(player.gameObject.transform.position);
+                    GL.Vertex(local);
                     GL.End();
                     GL.PopMatrix();
                 }
             }
-            static void CreateLineMaterial()
-            {
-                lineMaterial = new Material(Shader.Find("Hidden/Internal-Colored"));
-                lineMaterial.SetInt("_ZTest", 0);
-                lineMaterial.SetColor("Color", new Color(0x56, 0x00, 0xA5));
-                lineMaterial.renderQueue = 5000;
-
-            }
         }
     }
-
 }
