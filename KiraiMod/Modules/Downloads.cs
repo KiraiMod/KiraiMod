7dc071a961c86055411d88fd90f564cbd7ac640a
xKiraiChan <xKiraiChan@gmail.com>
2022-04-15 18:47:34 -0400

    add a clear option to download protection

--- KiraiMod/Modules/Downloads.cs
+++ KiraiMod/Modules/Downloads.cs
@@ -1,6 +1,7 @@
 ﻿using KiraiMod.Core.UI;
 using KiraiMod.Core.Utils;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
@@ -12,6 +13,12 @@ namespace KiraiMod.Modules
         public static Bound<bool> paused = new();
 
         private static MonoBehaviour downloadManager;
+        private static readonly PropertyInfo[] queues = Core.Types.DownloadManager.Type.GetProperties()
+            .Where(x => x.PropertyType.IsGenericType && x.PropertyType.GetGenericTypeDefinition() == typeof(Il2CppSystem.Collections.Generic.Queue<>))
+            .ToArray();
+
+        // in the future, this should use the inner type of all queues instead of the first one
+        private static readonly MethodInfo clear = typeof(Il2CppSystem.Collections.Generic.Queue<>).MakeGenericType(queues[0].PropertyType.GenericTypeArguments[0]).GetMethod("Clear");
 
         static Downloads()
         {
@@ -19,12 +26,15 @@ namespace KiraiMod.Modules
             {
                 UIGroup ui = new("Downloads", GUI.Groups.Protections);
                 ui.AddElement("Pause", paused);
-                //ui.AddElement("Clear").Changed += ClearDownloads;
+                ui.AddElement("Clear").Changed += Clear;
             };
 
-            Events.ApplicationStart += () => downloadManager = GameObject.Find("_Application/AssetBundleDownloadManager").GetComponents<MonoBehaviour>()[0];
+            Events.ApplicationStart += () => downloadManager = (MonoBehaviour)Core.Types.DownloadManager.Type.Singleton().GetValue(null);
 
             paused.ValueChanged += state => downloadManager.enabled = !state;
         }
+
+        // todo: print the amount of downloads cleared
+        public static void Clear() => queues.ForEach(queue => clear.Invoke(queue.GetValue(downloadManager), null));
     }
 }
