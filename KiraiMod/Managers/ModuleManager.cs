748df8fc35a0df8b9875e3ee501b39c29342a661
xKiraiChan <xKiraiChan@gmail.com>
2022-04-16 16:02:27 -0400

    module manager subscribe and remove unused package

--- KiraiMod/Managers/ModuleManager.cs
+++ KiraiMod/Managers/ModuleManager.cs
@@ -6,10 +6,12 @@ using System.Text.RegularExpressions;
 
 namespace KiraiMod.Managers
 {
+    // this needs to be rewritten eventually
     public static class ModuleManager
     {
         public static event Action<Dictionary<string, Type[]>> OnModuleRegistered;
 
+        public static Dictionary<string, Action<Type>[]> Subscriptions = new(); 
         public static Dictionary<string, Type[]> Modules = new();
 
         // in the future this class may be responsible for UI stuff, a la old kiraimod
@@ -18,13 +20,15 @@ namespace KiraiMod.Managers
         {
             Register(Assembly.GetExecutingAssembly());
 
-            foreach (Type module in Modules["Modules"])
+            Subscribe("Modules", module =>
+            {
                 if (!module.IsNested)
                 {
                     Plugin.log.LogDebug("Loading " + module.FullName);
-                    try { module.Initialize(); } 
+                    try { module.Initialize(); }
                     catch (Exception ex) { Plugin.log.LogError("Exception occurred whilst loading " + module.FullName + ": " + ex); }
                 }
+            });
         }
 
         public static void Register() => Register(Assembly.GetCallingAssembly());
@@ -43,9 +47,23 @@ namespace KiraiMod.Managers
                     if (Modules.TryGetValue(x.Key, out Type[] types))
                         Modules[x.Key] = types.Concat(x.Value).ToArray();
                     else Modules.Add(x.Key, x.Value);
+
+                    if (Subscriptions.TryGetValue(x.Key, out Action<Type>[] subs))
+                        x.Value.ForEach(type => subs.ForEach(sub => sub(type)));
                 });
 
             OnModuleRegistered?.Invoke(module);
+        }
+
+        public static void Subscribe(string module, Action<Type> action)
+        {
+            if (action is null) throw new ArgumentNullException(nameof(action));
+
+            Modules[module].ForEach(action);
+
+            if (Subscriptions.TryGetValue(module, out Action<Type>[] types))
+                Subscriptions[module] = types.Append(action).ToArray();
+            else Subscriptions.Add(module, new Action<Type>[1] { action });
         }
     }
 }
