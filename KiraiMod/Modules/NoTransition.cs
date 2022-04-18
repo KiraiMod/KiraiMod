589f51699cb8333637cbebbd01dd6d7e34d72c86
xKiraiChan <xKiraiChan@gmail.com>
2022-04-18 16:24:05 -0400

    module to remove world transition

---
+++ KiraiMod/Modules/NoTransition.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx.Configuration;
+using System;
+using UnityEngine;
+
+namespace KiraiMod.Modules
+{
+    // the idea for this comes from that person that was friends with keafy
+    // their mod had an issue, i think it was causing the game to crash
+    public static class NoTransition
+    {
+        public static ConfigEntry<bool> Enabled = Plugin.cfg.Bind("Visuals", "No Transitions", true, "Should the black fade between worlds be removed?");
+
+        public static GameObject Fade;
+
+        static NoTransition()
+        {
+            Events.UIManagerLoaded += () =>
+            {
+                Fade = GameObject.Find("UserInterface/PlayerDisplay/BlackFade/inverted_sphere");
+
+                Enabled.SettingChanged += ((EventHandler)((sender, args) => Fade.active = !Enabled.Value)).Invoke();
+            };
+        }
+    }
+}
