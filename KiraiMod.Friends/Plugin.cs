9ba866dba9132d630c702d05e9e387e29c44713a
xKiraiChan <xKiraiChan@gmail.com>
2022-05-17 22:02:29 -0400

    update to parts based moduleAPI

--- KiraiMod.Friends/Plugin.cs
+++ KiraiMod.Friends/Plugin.cs
@@ -3,6 +3,7 @@ using BepInEx.Configuration;
 using BepInEx.IL2CPP;
 using System;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 namespace KiraiMod.Friends
 {
@@ -21,7 +22,7 @@ namespace KiraiMod.Friends
         private void SetupRSA()
         {
             rsa = RSA.Create(4096);
-            ConfigEntry<string> entry = Config.Bind("Identity", "PrivateKey", Convert.ToBase64String(rsa.ExportRSAPrivateKey()));
+            ConfigEntry<string> entry = Config.Bind("Identity", "PrivateKey", "MIICCgKCAgEA" + Convert.ToBase64String(rsa.ExportParameters(true).Modulus));
             rsa.ImportRSAPrivateKey(Convert.FromBase64String(entry.Value), out int _);
         }
     }
